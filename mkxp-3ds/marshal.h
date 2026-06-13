d5157099bc17533c641b94a1886b45fbaa2ae3dc
godslayer <62102444+Disclored@users.noreply.github.com>
2026-06-13 16:03:11 +0100

    Delete mkxp-3ds/marshal.h

--- mkxp-3ds/marshal.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <mruby.h>
-#include <stdio.h>
-
-void      marshalBindingInit(mrb_state *mrb);
-void      marshalDumpInt(mrb_state *mrb, FILE *fp, mrb_value val);
-mrb_value marshalLoadInt(mrb_state *mrb, FILE *fp);
